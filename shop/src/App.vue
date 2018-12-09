e59101ada71aaae4235892c73fdefbaae807920c
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-12-09 01:14:20 -0500

    discovering basic js elements with vue

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -6,13 +6,11 @@
 </template>
 
 <script>
-  import { Test } from "./components/test"
-// import HelloWorld from './components/HelloWorld'
+  import Test from './components/Test'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    // HelloWorld
     Test
   }
 }
--- shop/src/components/Test.vue
+++ shop/src/components/Test.vue
@@ -1,13 +1,36 @@
 <template>
-  <div id="app">
-    <h1>Test</h1>
+  <div class="test">
+    <input type="text" v-model="title"><br>
+    <h1>{{title}}</h1>
+    <p>{{user.firstName+" "+user.lastName}}</p>
+    <p v-if="showName" v-text="user.firstName +' '+ user.lastName"></p>
+    <p v-else> No one</p>
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  components:{}
+  name: 'test',
+  data(){
+    return{
+      title: "WHO IS TALKING ABOUT ALEXA",
+      user: {
+        firstName: 'John',
+        lastName: 'Casey'
+
+      },
+      showName: true,
+      items:[
+        {title:'item one'},
+        {title:'item two'},
+        {title:'item three'}
+
+      ]
+    }
+  }
 }
 </script>
 <style scoped>
