f8e2bd51b5303b61923a23a683e204cdd965ac42
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-12-09 00:40:49 -0500

    Cha Ching. I got Vue to behave for me. Now I am familirizing myself and making sure I can use it and understand so we can then move onto docker then move onto PWA then move into the vue store

---
+++ shop/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <test></test>
+  </div>
+</template>
+
+<script>
+  import { Test } from "./components/test"
+// import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    Test
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ shop/src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>Test</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components:{}
+}
+</script>
+<style scoped>
+
+</style>
+
+
