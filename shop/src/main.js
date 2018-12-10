e8a446ee9ca027c6cfeab47000756f0379ac2a1a
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-12-09 23:11:54 -0500

    finished initial overview of Vue

--- shop/src/main.js
+++ shop/src/main.js
@@ -2,15 +2,35 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import vueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 import App from './App'
+import Users from './components/Users'
+import Test from './components/Test'
+
 
 Vue.use(vueResource)
+Vue.use(VueRouter)
 
-Vue.config.productionTip = false
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {path: '/', component: Users},
+    {path: '/test', component: Test}
+  ]
+});
+// Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+  router,
+  template: `
+  <div id="app">
+    <ul>
+      <li><router-link to="/">Users</router-link></li>
+      <li><router-link to="/Test">Test</router-link></li>
+    </ul>
+    <router-view></router-view>
+    </div>
+  `,
+}).$mount('#app')
